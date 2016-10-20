4515ae92bbc4f9973ec9ce4029d11a6bf66d90bf
rosariom27 <rosariomolloy@gmail.com>
2016-10-20 16:30:32 -0300

    20/10
    Mitad del lab 6

---
+++ 2016/UI.Web/Usuarios.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Usuarios.aspx.cs" Inherits="UI.Web.Usuarios" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Usuarios</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Panel ID="gridPanel" runat="server">
+            <asp:GridView ID="gridView" runat="server" AutoGenerateColumns="false" 
+              SelectedRowStyle-BackColor="Black"
+              SelectedRowStyle-ForeColor="White"
+              DataKeyNames="ID" OnSelectedIndexChanged="gridView_SelectedIndexChanged"  >
+              <Columns>
+                  <%--<asp:BoundField HeaderText="Nombre" DataField="Nombre" />
+                  <asp:BoundField HeaderText="Apellido" DataField="Apellido" />
+                  <asp:BoundField HeaderText="EMail" DataField="EMail" />--%>
+                  <asp:BoundField HeaderText="Usuario" DataField="NombreUsuario" />
+                  <asp:BoundField HeaderText="Habilitado" DataField="Habilitado" />
+                  <asp:CommandField SelectText="Seleccionar" ShowSelectButton="True" />
+              </Columns>
+            </asp:GridView>
+        </asp:Panel>
+        <asp:Panel ID="formPanel" Visible="false" runat="server" >
+            <asp:Label ID="habilitadoLabel" runat="server" Text="Habilitado: "></asp:Label>
+            <asp:CheckBox ID="habilitadoCheckBox" runat="server"></asp:CheckBox>
+            <br />
+            <asp:Label ID="nombreUsuarioLabel" runat="server" Text="Usuario: "></asp:Label>
+            <asp:TextBox ID="nombreUsuarioTextBox" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="claveLabel" runat="server" Text="Clave: "></asp:Label>
+            <asp:TextBox ID="claveTextBox" TextMode="Password" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="repetirClaveLabel" runat="server" Text="Repetir Clave: "></asp:Label>
+            <asp:TextBox ID="repetirClaveTextBox" TextMode="Password" runat="server"></asp:TextBox>
+            <br />
+        </asp:Panel>
+
+        <asp:Panel ID="gridActionsPanel" runat="server">
+            <asp:LinkButton ID="editarLinkButton" runat="server" OnClick="editarLinkButton_Click">Editar</asp:LinkButton>
+            <asp:LinkButton ID="eliminarLinkButton" runat="server">Eliminar</asp:LinkButton>
+            <asp:LinkButton ID="nuevoLinkButton" runat="server">Nuevo</asp:LinkButton>
+        </asp:Panel>
+
+        <asp:Panel ID="formActionsPanel" runat="server">
+
+            <asp:LinkButton ID="aceptarLinkButton" runat="server" OnClick="aceptarLinkButton_Click">Aceptar</asp:LinkButton>
+            <asp:LinkButton ID="cancelarLinkButton" runat="server">Cancelar</asp:LinkButton>
+        </asp:Panel>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ 2016/UI.Web/Usuarios.aspx.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Entidades;
+using Negocio;
+
+namespace UI.Web
+{
+    public partial class Usuarios : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                this.LoadGrid();
+            }
+        }
+
+       UsuarioLogic _logic;
+       public UsuarioLogic Logic
+       {
+           get { if (_logic == null)
+           {
+               _logic = new UsuarioLogic();
+           }
+           return _logic;
+           }
+       }
+
+       private void LoadGrid()
+       {
+           this.gridView.DataSource = this.Logic.GetAll();
+           this.gridView.DataBind();
+       }
+
+       public enum FormModes
+       {
+           Alta,
+           Baja,
+           Modificacion
+       }
+
+        public FormModes FormMode
+       {
+           get { return(FormModes) this.ViewState["FormMode"] ;}
+           set { this.ViewState["FormMode"] = value;}
+
+       }
+
+        private Usuario Entidad
+        {
+            get;
+            set;
+        }
+
+        private int SelectedID
+        {
+            get {
+
+                if (this.ViewState["SelectedID"] != null)
+                {
+                    return (int)this.ViewState["SelectedID"];
+
+                }
+                else
+                {
+                    return 0;
+                }
+                           
+                }
+            set { this.ViewState["SelectedID"] = value; }
+
+        }
+
+        private bool IsEntitySelected
+        {
+            get { return (this.SelectedID != 0); }
+        }
+
+        protected void gridView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.SelectedID = (int)this.gridView.SelectedValue;
+        }
+
+        private void LoadForm(int id)
+        {
+            this.Entidad = this.Logic.GetOne(id);
+            this.nombreUsuarioTextBox.Text = this.Entidad.NombreUsuario;
+            this.habilitadoCheckBox.Checked = this.Entidad.Habilitado;
+
+        }
+
+        protected void editarLinkButton_Click(object sender, EventArgs e)
+        {
+            if(this.IsEntitySelected)
+            {
+                this.formPanel.Visible = true;
+                this.FormMode = FormModes.Modificacion;
+                this.LoadForm(this.SelectedID);
+            }
+        }
+
+        private void LoadEntidad(Usuario usuario)
+        {
+            usuario.NombreUsuario = this.nombreUsuarioTextBox.Text;
+            usuario.Habilitado = this.habilitadoCheckBox.Checked;
+
+        }
+
+        private void SaveEntidad(Usuario usuario)
+        {
+            this.Logic.Save(usuario);
+        }
+
+        protected void aceptarLinkButton_Click(object sender, EventArgs e)
+        {
+            this.Entidad = new Usuario();
+            this.Entidad.ID = this.SelectedID;
+            this.Entidad.State = Entidades.Entidad.States.Modified;
+            this.LoadEntidad(this.Entidad);
+            this.SaveEntidad(this.Entidad);
+            this.LoadGrid();
+
+            this.formPanel.Visible = false;
+        }
+
+    }
+}
